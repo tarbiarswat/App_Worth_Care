3f29f0a8494298648aa892766e2b412f9dc4ee0d
Tarbiar Swat <tarbiar.swat@northsouth.edu>
2019-11-09 02:58:56 +0600

    Firebase Authentication added Login to Registration Page onClickListener added

--- WorthCare/app/src/main/java/com/worthcare/LoginActivity.java
+++ WorthCare/app/src/main/java/com/worthcare/LoginActivity.java
@@ -2,13 +2,26 @@ package com.worthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView gotoRegActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        gotoRegActivity = findViewById(R.id.goto_regactivity);
+        gotoRegActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_gotoRegActivity = new Intent(LoginActivity.this,RegActivity.class);
+                startActivity(intent_gotoRegActivity);
+            }
+        });
     }
 }
