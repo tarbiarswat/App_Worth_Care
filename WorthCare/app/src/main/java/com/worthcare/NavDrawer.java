71383fca4a2c997f9c8eb5c71d00b0fc6d904cce
Tarbiar Swat <tarbiar.swat@northsouth.edu>
2019-11-26 05:05:19 +0600

    Redirected to all fragments, and Log out functionality added

--- WorthCare/app/src/main/java/com/worthcare/NavDrawer.java
+++ WorthCare/app/src/main/java/com/worthcare/NavDrawer.java
@@ -1,15 +1,23 @@
 package com.worthcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class NavDrawer extends AppCompatActivity {
+public class NavDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +28,60 @@ public class NavDrawer extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_medicine_reminder:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MedicineReminderFragment()).commit();
+                break;
+            case R.id.nav_consultation:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ConsultationFragment()).commit();
+                break;
+            case R.id.nav_bmi_calculator:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BmiCalculatorFragment()).commit();
+                break;
+            case R.id.nav_medicine_directory:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MedicineDirectoryFragment()).commit();
+                break;
+            case R.id.nav_notifications:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NotificationsFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_privacy_policy:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PrivacyPolicyFragment()).commit();
+                break;
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                showDialogue("You are signed out...");
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -35,5 +92,9 @@ public class NavDrawer extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    }
+    private void showDialogue(String message)
+    {
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
     }
 }
